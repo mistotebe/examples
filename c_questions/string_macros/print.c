4731d8f54935192040d900f338d34b555ba04df1
Ondřej Kuzník <ondra@mistotebe.net>
2014-11-02 20:16:05 +0000

    More macros

--- c_questions/string_macros/print.c
+++ c_questions/string_macros/print.c
@@ -1,8 +1,13 @@
 #include <stdio.h>
 
+#define fragment a, b
+#define receptor(first, ...) printf(#first " - " #__VA_ARGS__ "\n")
+#define donor(x) receptor((x))
+
 int main()
 {
   printf(STRING_MACRO);
+  donor(fragment);
 
   return 0;
 }
