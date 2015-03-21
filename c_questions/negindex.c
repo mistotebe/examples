3073e9a1138c6095a8117b5ba4752f6ddc268479
Ondřej Kuzník <ondra@mistotebe.net>
2015-03-21 22:07:26 +0000

    Warnings

--- c_questions/negindex.c
+++ c_questions/negindex.c
@@ -3,7 +3,7 @@
 int main()
 {
   const char *string = "0123456789";
-  char *ptr = string + 4;
+  const char *ptr = string + 4;
   printf("-9 %% 4 = %d\n", -9 % 4);
   printf("ptr[0] = '%c' = string[4] = '%c'\n", ptr[0], string[4]);
   printf("ptr[-2] = '%c' = string[2] = '%c'\n", ptr[-2], string[2]);
--- c_questions/pointer.c
+++ c_questions/pointer.c
@@ -3,14 +3,14 @@
 int main()
 {
   void *ptr = (void *)0x1;
-  while (ptr = (void *)((long long) ptr << 0x1))
-    printf("\r!0x%llx does not hold", ptr);
-  printf("\n!0x%llx holds\n", ptr);
+  while ((ptr = (void *)((long long) ptr << 0x1)))
+    printf("\r!0x%p does not hold", ptr);
+  printf("\n!0x%p holds\n", ptr);
 
   ptr = (void *)0x1;
   while ((ptr = (void *)((long long) ptr << 0x1)) != NULL)
-    printf("\r(0x%llx != NULL) does not hold", ptr);
-  printf("\n(0x%llx != NULL) holds\n", ptr);
+    printf("\r(0x%p != NULL) does not hold", ptr);
+  printf("\n(0x%p != NULL) holds\n", ptr);
 
   return 0;
 }
